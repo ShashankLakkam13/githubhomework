1478296bf47a066b2c62b0337e2146d6c8cdbcbc
shashank Reddy <shashank970613@gmail.com>
2023-08-30 16:21:05 -0400

    adding files

---
+++ src/main/java/in/shashank/GitHubHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package in.shashank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubHomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubHomeWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/shashank/GitHubHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package in.shashank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubHomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
